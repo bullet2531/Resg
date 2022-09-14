1e630f958f0343089750026d34fdbde58188019d
bullet2531 <ashishadake72@gmail.com>
2022-09-14 22:10:23 +0530

    first commit

---
+++ UserR/Controllers/UserApiController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using UserR.Models;
+
+namespace UserR.Controllers
+{
+    public class UserApiController : ApiController
+    {
+        UserDBEntities db = new UserDBEntities();
+        [HttpGet]
+        public IHttpActionResult GetUser()
+        {
+            List<User> Usr = db.Users.ToList();
+            return Ok(Usr);
+        }
+        [HttpGet]
+        public IHttpActionResult GetUserById(int id)
+        {
+            var usr = db.Users.Where(model => model.UserId == id).FirstOrDefault();
+            return Ok(usr);
+        }
+
+        [HttpPost]
+        public IHttpActionResult AddUser(User u)
+        {
+            db.Users.Add(u);
+            db.SaveChanges();
+            return Ok();
+        }
+        [HttpPut]
+        public IHttpActionResult UpdateUser(User u)
+        {
+            var usr = db.Users.Where(model => model.UserId == u.UserId).FirstOrDefault();
+            if (usr != null)
+            {
+                usr.UserId = u.UserId;
+                usr.FullName = u.FullName;
+                usr.Email = u.Email;
+                usr.Location = u.Location;
+                usr.Language = u.Language;
+                usr.Password = u.Password;
+                usr.Role = u.Role;
+                usr.Userpin = u.Userpin;
+                db.SaveChanges();
+
+            }
+            else
+            {
+                return NotFound();
+            }
+            return Ok();
+        }
+    }
+}
---
+++ UserR/Controllers/UserMvcController.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using System.Web.Mvc;
+using UserR.Models;
+
+namespace UserR.Controllers
+{
+    public class UserMvcController : Controller
+    {
+        //
+        // GET: /UserMvc/
+        HttpClient client = new HttpClient();
+        public ActionResult Index()
+        {
+            List<User> userlist = new List<User>();
+            client.BaseAddress = new Uri("http://localhost:57352/api/UserApi");
+            var response = client.GetAsync("UserApi");
+            response.Wait();
+
+            var test = response.Result;
+            if (test.IsSuccessStatusCode)
+            {
+                var display = test.Content.ReadAsAsync<List<User>>();
+                display.Wait();
+                userlist = display.Result;
+            }
+            return View(userlist); 
+        }
+        public ActionResult Login()
+        {
+            return View();
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(User u)
+        {
+            client.BaseAddress = new Uri("http://localhost:57352/api/UserApi");
+            var response = client.PostAsJsonAsync("UserApi", u);
+            response.Wait();
+
+            var test = response.Result;
+
+            if (test.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+
+
+            return View("Create");
+        }
+        public ActionResult Details(int id)
+        {
+            User u = null;
+            client.BaseAddress = new Uri("http://localhost:57352/api/UserApi");
+            var response = client.GetAsync("UserApi?id=" + id.ToString());
+            response.Wait();
+
+            var test = response.Result;
+            if (test.IsSuccessStatusCode)
+            {
+                var display = test.Content.ReadAsAsync<User>();
+                display.Wait();
+                u = display.Result;
+            }
+            return View(u);
+        }
+        public ActionResult Edit(int id)
+        {
+            User u = null;
+            client.BaseAddress = new Uri("http://localhost:57352/api/UserApi");
+            var response = client.GetAsync("UserApi?id=" + id.ToString());
+            response.Wait();
+
+            var test = response.Result;
+            if (test.IsSuccessStatusCode)
+            {
+                var display = test.Content.ReadAsAsync<User>();
+                display.Wait();
+                u = display.Result;
+            }
+            return View(u);
+        }
+        [HttpPost]
+        public ActionResult Edit(User u)
+        {
+            client.BaseAddress = new Uri("http://localhost:57352/api/UserApi");
+            var response = client.PutAsJsonAsync("UserApi", u);
+            response.Wait();
+
+            var test = response.Result;
+            if (test.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+
+
+            return View("Edit");
+        }
+
+	}
+}
